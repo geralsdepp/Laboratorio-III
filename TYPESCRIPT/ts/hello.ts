7fc12bda00a1aa2617883df52fed75269d03263a
alumno <alumno@utnfra.com.ar>
2019-11-12 21:41:54 -0300

    egeegb

--- TYPESCRIPT/ts/hello.ts
+++ TYPESCRIPT/ts/hello.ts
@@ -1,7 +1,7 @@
 /// <reference path="bye.ts"/>
 
 let mensaje : string = "Hola que tal";
-console.log(mensaje);
+//console.log(mensaje);
 
 //array
 
@@ -17,7 +17,7 @@ enum EHeroes{
     HombreAraña
 }
 
-console.log("Enum..");
+//console.log("Enum..");
 //for (const key in EHeroes) {
    /// console.log(key);
 //}
@@ -25,10 +25,147 @@ console.log("Enum..");
 
 //Funciones
 
-let funcionEnviarMision = function(heroe : string = "Spiderman") : string{
+/*let funcionEnviarMision = function(heroe : string = "Spiderman") : string{
 
     return heroe = heroe + " enviado";
+}*/
+
+//let retorno : string = funcionEnviarMision();
+//console.log(retorno);
+
+
+// PARAMETROS REST
+
+let funcionEnviarMision2 = function(...heroes:string[]):void{
+    for (let i = 0; i < heroes.length; i++) {
+        console.log(heroes[i] + " enviado");
+        
+    }
 }
 
-let retorno : string = funcionEnviarMision();
-console.log(retorno);
+funcionEnviarMision2("Spiderman", "Batman", "Guason");
+
+//funcion flecha
+
+let funcionEnviarMision3 = (heroe: string = "Heroe"):string=>{
+    return heroe + " enviado a misión 3";
+}
+
+console.log(funcionEnviarMision3());
+
+//tipo en el objeto
+
+let flash:{nombre: string, edad: number, poderes:string[], getNombre:()=>string} = {
+    nombre: "Barry Allen",
+    edad: 24,
+    poderes: ["Corre", "Viaja en el tiempo"],
+    getNombre(){
+        return this.nombre;
+    }
+}
+
+//tipo personalizado
+
+type Heroe = {nombre: string, edad:number, poderes:string[], getNombre:()=>string};
+let ironman:Heroe = {
+    nombre: "Tony Stark",
+    edad: 24,
+    poderes: ["vuela"],
+    getNombre:function(){return this.nombre;}
+}
+console.log(ironman.getNombre());
+
+//Interfaces
+interface IHeroe{
+    nombre: string,
+    poder?:string,
+    mostrar?():string
+}
+
+let Wolverine:IHeroe = {
+    nombre: "James",
+
+}
+
+console.log(Wolverine.nombre);
+
+// Interface en clases
+
+class Avenger implements IHeroe{
+    nombre: string = "Batman"
+}
+
+class Mutante implements IHeroe{
+    nombre:string = "un mutante"
+}
+
+let unAvenger = new Avenger();
+let unMutante = new Mutante();
+
+console.log(unAvenger.nombre);
+console.log(unMutante.nombre);
+
+//interface en funcion
+
+interface IfuncDosNumeros{
+    (num1:number, num2:number):number;
+}
+
+let miFuncion: IfuncDosNumeros;
+miFuncion = (num1:number, num2:number)=>num1+num2;
+console.log(miFuncion(2,10));
+
+//clases
+
+class Avenger2 implements IHeroe{
+    nombre:string = "un avanger";
+
+    constructor(nombre:string){
+        this.nombre = nombre;
+    }
+}
+
+let av2 = new Avenger2("Hulk");
+console.log(av2.nombre);
+
+//clase con atrib privado
+class Avenger3{
+    private _nombre:string = "un avenger";
+    private _edad : number = 0;
+
+    constructor(nombre:string, edad:number){
+        this._nombre = nombre;
+        this._edad = edad;
+    }
+    
+    public get edad() : number {
+        return this._edad
+    }
+    
+    public set edad(edad : number) {
+        this._edad = edad;
+    }
+
+    public mostrar = ()=>this._nombre;
+    
+}
+
+let av3 = new Avenger3("Ironman", 30);
+console.log("Clases 2: " + av3.mostrar());
+av3.edad = 40;
+console.log("Clases 2: " + av3.edad);
+
+//Clases con metodos estaticos
+class Xmen{
+    static nombre_de_clase = "Xmen";
+}
+console.log("atributo estatico: " + Xmen.nombre_de_clase);
+
+//Herencia
+
+class AvengerHeredado extends Avenger2{
+
+}
+
+let avh = new AvengerHeredado("Heredado");
+console.log(avh.nombre);
--- TYPESCRIPT/ts/output.js
+++ TYPESCRIPT/ts/output.js
@@ -1,7 +1,7 @@
 "use strict";
 /// <reference path="bye.ts"/>
 let mensaje = "Hola que tal";
-console.log(mensaje);
+//console.log(mensaje);
 //array
 let vector = [1, 2, 3, 4];
 //Tupla
@@ -12,17 +12,107 @@ var EHeroes;
     EHeroes[EHeroes["Batman"] = 0] = "Batman";
     EHeroes[EHeroes["HombreAra\u00F1a"] = 1] = "HombreAra\u00F1a";
 })(EHeroes || (EHeroes = {}));
-console.log("Enum..");
+//console.log("Enum..");
 //for (const key in EHeroes) {
 /// console.log(key);
 //}
 //console.log(EHeroes[EHeroes.Batman]);
 //Funciones
-let funcionEnviarMision = function (heroe = "Spiderman") {
+/*let funcionEnviarMision = function(heroe : string = "Spiderman") : string{
+
     return heroe = heroe + " enviado";
+}*/
+//let retorno : string = funcionEnviarMision();
+//console.log(retorno);
+// PARAMETROS REST
+let funcionEnviarMision2 = function (...heroes) {
+    for (let i = 0; i < heroes.length; i++) {
+        console.log(heroes[i] + " enviado");
+    }
 };
-let retorno = funcionEnviarMision();
-console.log(retorno);
+funcionEnviarMision2("Spiderman", "Batman", "Guason");
+//funcion flecha
+let funcionEnviarMision3 = (heroe = "Heroe") => {
+    return heroe + " enviado a misión 3";
+};
+console.log(funcionEnviarMision3());
+//tipo en el objeto
+let flash = {
+    nombre: "Barry Allen",
+    edad: 24,
+    poderes: ["Corre", "Viaja en el tiempo"],
+    getNombre() {
+        return this.nombre;
+    }
+};
+let ironman = {
+    nombre: "Tony Stark",
+    edad: 24,
+    poderes: ["vuela"],
+    getNombre: function () { return this.nombre; }
+};
+console.log(ironman.getNombre());
+let Wolverine = {
+    nombre: "James",
+};
+console.log(Wolverine.nombre);
+// Interface en clases
+class Avenger {
+    constructor() {
+        this.nombre = "Batman";
+    }
+}
+class Mutante {
+    constructor() {
+        this.nombre = "un mutante";
+    }
+}
+let unAvenger = new Avenger();
+let unMutante = new Mutante();
+console.log(unAvenger.nombre);
+console.log(unMutante.nombre);
+let miFuncion;
+miFuncion = (num1, num2) => num1 + num2;
+console.log(miFuncion(2, 10));
+//clases
+class Avenger2 {
+    constructor(nombre) {
+        this.nombre = "un avanger";
+        this.nombre = nombre;
+    }
+}
+let av2 = new Avenger2("Hulk");
+console.log(av2.nombre);
+//clase con atrib privado
+class Avenger3 {
+    constructor(nombre, edad) {
+        this._nombre = "un avenger";
+        this._edad = 0;
+        this.mostrar = () => this._nombre;
+        this._nombre = nombre;
+        this._edad = edad;
+    }
+    get edad() {
+        return this._edad;
+    }
+    set edad(edad) {
+        this._edad = edad;
+    }
+}
+let av3 = new Avenger3("Ironman", 30);
+console.log("Clases 2: " + av3.mostrar());
+av3.edad = 40;
+console.log("Clases 2: " + av3.edad);
+//Clases con metodos estaticos
+class Xmen {
+}
+Xmen.nombre_de_clase = "Xmen";
+console.log("atributo estatico: " + Xmen.nombre_de_clase);
+//Herencia
+class AvengerHeredado extends Avenger2 {
+}
+let avh = new AvengerHeredado("Heredado");
+console.log(avh.nombre);
 /// <reference path="hello.ts"/>
 let mens = "Chau";
 console.log(mens);
